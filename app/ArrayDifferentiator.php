b3d3f6cb6fe386dffd60c45499bfce4fbeaac562
Ryan Miller <ryanmillergm@gmail.com>
2024-08-09 18:33:16 -0600

    Adds Array Differentiator function - test passing

--- app/ArrayDifferentiator.php
+++ app/ArrayDifferentiator.php
@@ -4,4 +4,17 @@ namespace App;
 
 class ArrayDifferentiator {
 
+    public function arrayDiff($a, $b)
+    {
+        // best answer:
+        return array_values(array_diff($a, $b));
+
+        // My Answer:
+        // $diff = array_diff($a, $b);
+        // $result = [];
+        // foreach ($diff as $value) {
+        //     array_push($result, $value);
+        // }
+        // return $result;
+    }
 }
