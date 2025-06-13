5872b5e2e50d49a3e40e2eecfddcc0c8151cb780
Ryan Miller <ryanmillergm@gmail.com>
2025-06-13 08:08:38 -0600

    Adds simple calculator test challenge

--- app/CalculatorSimple.php
+++ app/CalculatorSimple.php
@@ -4,8 +4,31 @@ namespace App;
 
 class CalculatorSimple {
     public function calculator($num1, $num2, $operator)
-    {
-        return 'something';
+    {        
+        if (!is_numeric($num1) || !is_numeric($num2) || is_string($num1) || is_string($num2)) {
+            return "unknown value";
+        }
+
+        switch ($operator) {
+            case "+":
+                $calculation = ($num1 + $num2);
+                return $calculation;
+                break;
+            case "-":
+                $calculation = ($num1 - $num2);
+                return $calculation;
+                break;
+            case "*":
+                $calculation = ($num1 * $num2);
+                return $calculation;
+                break;
+            case "/":
+                $calculation = ($num1 / $num2);
+                return $calculation;
+                break;
+            default:
+                return "unknown value";
+            }
     }
 }
 
