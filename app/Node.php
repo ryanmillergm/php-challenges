ec7818e5ac01d09cc961af96d5b127a8a0832075
Ryan Miller <ryanmillergm@gmail.com>
2025-06-18 11:00:00 -0600

    Adds createChain instance method to Node class

--- app/Node.php
+++ app/Node.php
@@ -11,6 +11,26 @@ class Node {
         $this->data = $data;
         $this->next = null;
     }
+
+    public static function createChain($dangling_length, $loop_length) {
+        $nodes = [];
+
+        for ($i = 0; $i < $dangling_length + $loop_length; $i++) {
+            $nodes[] = new Node();
+        }
+
+        for ($i = 0; $i < count($nodes) - 1; $i++) {
+            $nodes[$i]->next = $nodes[$i + 1];
+        }
+
+        $nodes[count($nodes) - 1]->next = $nodes[$dangling_length]; 
+
+        return $nodes[0];
+    }
+
+    public function getNext(): ?Node {
+        return $this->next;
+    }
 }
 
 ?>
